45ccc514ee2e0eceecaa13db0b56911ec0f9f893
unknown <biramantvydas@gmail.com>
2024-11-24 16:44:15 +0200

    Rename files to fit format

--- public/register.php
+++ public/register.php
@@ -3,5 +3,5 @@
     require_once '../src/controllers/EventController.php';
     $controllerEvent = new EventController($dbc);
     $data =  $controllerEvent->getCreateEventData();
-    require_once '../src/views/register_form.php';
+    require_once '../src/views/register_view.php';
 ?>
